391f09b640ca39e1dd98ff1cc65af79048d7c0f5
Harrison Barker <harrison@thebarkers.me.uk>
2022-01-22 19:20:54 +0000

    Creative commons permissive copyright
    CRPL-29 Creative commons contract

---
+++ CRPL.Contracts/contracts/Copyrights/Permissive.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./CopyrightWithMeta.sol";
+import "../Utils/IdCounters.sol";
+
+/// @dev Implementation of a traditional copyright contract using CopyrightBase
+contract Permissive is CopyrightWithMeta {
+    using IdCounters for IdCounters.IdCounter;
+
+    constructor() CopyrightWithMeta("Standard Copyright", "https://creativecommons.org/licenses/by/2.0/") payable {}
+}
