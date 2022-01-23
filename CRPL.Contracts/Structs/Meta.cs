84ba1ce79ff979b50bc1ee014d0da2abf85527c3
Harrison Barker <harrison@thebarkers.me.uk>
2022-01-23 10:16:21 +0000

    Compiled C# classes for smart contracts

---
+++ CRPL.Contracts/Structs/Meta.cs
@@ -0,0 +1,23 @@
+using System.Numerics;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+
+namespace CRPL.Contracts.Structs
+{
+    public partial class Meta : MetaBase { }
+
+    public class MetaBase 
+    {
+        [Parameter("string", "title", 1)]
+        public virtual string Title { get; set; }
+        [Parameter("uint256", "expires", 2)]
+        public virtual BigInteger Expires { get; set; }
+        [Parameter("uint256", "registered", 3)]
+        public virtual BigInteger Registered { get; set; }
+        [Parameter("string", "workHash", 4)]
+        public virtual string WorkHash { get; set; }
+        [Parameter("string", "workUri", 5)]
+        public virtual string WorkUri { get; set; }
+        [Parameter("string", "legalMeta", 6)]
+        public virtual string LegalMeta { get; set; }
+    }
+}
---
+++ CRPL.Contracts/Structs/RestructureProposal.cs
@@ -0,0 +1,14 @@
+using Nethereum.ABI.FunctionEncoding.Attributes;
+
+namespace CRPL.Contracts.Structs
+{
+    public partial class RestructureProposal : RestructureProposalBase { }
+
+    public class RestructureProposalBase 
+    {
+        [Parameter("tuple[]", "oldStructure", 1)]
+        public virtual List<OwnershipStructure> OldStructure { get; set; }
+        [Parameter("tuple[]", "newStructure", 2)]
+        public virtual List<OwnershipStructure> NewStructure { get; set; }
+    }
+}
