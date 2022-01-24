57ce016e609b4b9bc3b1e34c7f3af965ffb924b7
Harrison Barker <harrison@thebarkers.me.uk>
2022-01-24 22:25:28 +0000

    Test environment setup with db factory

---
+++ CRPL.Tests/Factories/TestDbApplicationContextFactory.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using CRPL.Data.Account;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRPL.Tests.Factories;
+
+public class TestDbApplicationContextFactory : IDisposable
+{
+    private DbConnection? Connection;
+
+    private DbContextOptions<ApplicationContext> CreateOptions()
+    {
+        return new DbContextOptionsBuilder<ApplicationContext>()
+            .EnableSensitiveDataLogging()
+            .UseSqlite(Connection).Options;
+    }
+
+    public ApplicationContext CreateContext()
+    {
+        if (Connection == null)
+        {
+            Connection = new SqliteConnection("DataSource=:memory:;Foreign Keys=False");
+            Connection.Open();
+
+            var options = CreateOptions();
+            using (var context = new ApplicationContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+                seed(context);
+            }
+        }
+
+        return new ApplicationContext(CreateOptions());
+    }
+
+
+    private void seed(ApplicationContext context)
+    {
+        List<UserAccount> userAccounts = new List<UserAccount>()
+        {
+            new()
+            {
+                Id = new Guid("D67B16A9-2E44-4A14-9169-0AE8FED2203C"),
+                Email = null,
+                Status = UserAccount.AccountStatus.Incomplete,
+                FirstName = "Incomplete",
+                LastName = "User",
+                PhoneNumber = null,
+                RegisteredJurisdiction = null,
+                DateOfBirth = null
+            },
+            new()
+            {
+                Id = new Guid("73C4FF17-1EF8-483C-BCDB-9A6191888F04"),
+                Email = "mail@harrisonbarker.co.uk",
+                Status = UserAccount.AccountStatus.Complete,
+                FirstName = "Complete",
+                LastName = "User",
+                PhoneNumber = "+4407852276048",
+                RegisteredJurisdiction = "GBR",
+                DateOfBirth = new UserAccount.DOB()
+                {
+                    Year = 2000, Month = 7, Day = 24
+                }
+            },
+            new()
+            {
+                Id = new Guid("8E9C6FB8-A8D7-459F-A39C-B06E68FE4E03"),
+                Email = "",
+                Status = UserAccount.AccountStatus.Created,
+                FirstName = "",
+                LastName = "",
+                PhoneNumber = "",
+                RegisteredJurisdiction = "",
+                DateOfBirth = new UserAccount.DOB()
+            },
+        };
+        
+        context.UserAccounts.AddRange(userAccounts);
+        context.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+        if (Connection != null)
+        {
+            Connection.Dispose();
+            Connection = null;
+        }
+    }
+}
--- CRPL.Tests/TestConstants.cs
+++ CRPL.Tests/TestConstants.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using CRPL.Data;
+using CRPL.Data.Account;
 using CRPL.Data.BlockchainUtils;
 using Nethereum.Web3.Accounts;
 
@@ -8,6 +11,11 @@ public class TestConstants
 {
     public static string TestAccountId = "0x12890d2cce102216644c59dae5baed380d84830c";
     public static string PrivateKey = "0xb5b1870957d373ef0eeffecc6e4812c0fd08f554b37b233526acc331bf1544f7";
-    
+
     public static BlockchainConnection PrivateTestConnection() => new BlockchainConnection(LawsOfNature.ChainUrl, new Account(PrivateKey, LawsOfNature.ChainId));
+
+    public static Dictionary<UserAccount.AccountStatus, Guid> TestAccountIds = new()
+    {
+        { UserAccount.AccountStatus.Incomplete, new Guid("D67B16A9-2E44-4A14-9169-0AE8FED2203C") }
+    };
 }
