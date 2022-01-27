8117a182958326d817436dc553b061ca2e1daca1
Harrison Barker <harrison@thebarkers.me.uk>
2022-01-27 15:39:32 +0000

    phone dial code db migrations

---
+++ CRPL.Web/Migrations/Application/20220127152139_phoneDialCodeAdded.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRPL.Web.Migrations.Application
+{
+    public partial class phoneDialCodeAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DialCode",
+                table: "UserAccounts",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DialCode",
+                table: "UserAccounts");
+        }
+    }
+}
