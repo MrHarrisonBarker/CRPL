cf372ab3eaa2f0a87f44d3394a68cefd470ea2f6
Harrison Barker <harrison@thebarkers.me.uk>
2022-02-02 03:55:34 +0000

    pipelines updates, added new HardHat project for smart contract testing with pipeline yml

---
+++ CRPL.Contracts/hardhat.config.ts
@@ -0,0 +1,21 @@
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.4"
+}
+
+export default config;
