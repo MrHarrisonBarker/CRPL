7b662acad149e87ccde4c1e9fbfa5d79067eeed5
Harrison Barker <harrison@thebarkers.me.uk>
2022-02-06 21:14:33 +0000

    Refactored ownership structure and stake to use form arrays

--- CRPL.Web/ClientApp/src/app/Forms/ownership-stake/ownership-stake.component.ts
+++ CRPL.Web/ClientApp/src/app/Forms/ownership-stake/ownership-stake.component.ts
@@ -1,124 +1,56 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {
   AbstractControl,
-  ControlValueAccessor,
+  ControlValueAccessor, FormArray, FormGroup,
   NG_VALIDATORS, NG_VALUE_ACCESSOR, ValidationErrors
 } from "@angular/forms";
 import {OwnershipStake, OwnershipStakeInput} from "../../_Models/StructuredOwnership/OwnershipStake";
+import {UserService} from "../../_Services/user.service";
+import {UserAccountMinimalViewModel} from "../../_Models/Account/UserAccountMinimalViewModel";
 
 @Component({
-  selector: 'ownership-stake-input [isDefault]',
+  selector: 'ownership-stake-input',
   templateUrl: './ownership-stake.component.html',
   styleUrls: ['./ownership-stake.component.css'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      multi: true,
-      useExisting: OwnershipStakeComponent
-    },
-    {
-      provide: NG_VALIDATORS,
-      multi: true,
-      useExisting: OwnershipStakeComponent
-    },
-  ]
 })
-export class OwnershipStakeComponent implements OnInit, ControlValueAccessor
+export class OwnershipStakeComponent implements OnInit
 {
+  @Input() Index!: number;
+  @Input() public Form!: FormGroup;
+
   @Input() isDefault: boolean = false;
   @Input() MaximumShares: number = 100;
-  @Input() Disabled = false;
 
-  @Output() DestroyEmit: EventEmitter<any> = new EventEmitter<any>();
+  // @Output() DestroyEmit: EventEmitter<any> = new EventEmitter<any>();
 
   ngOnInit (): void
   {
+    console.log("loaded stake!", this.Form)
   }
 
-  constructor ()
+  constructor (private userService: UserService)
   {
   }
 
-  onChange: any = () =>
+  get IsValid()
   {
-  }
-
-  onTouch: any = () =>
-  {
-  }
-
-  public OwnershipStake: OwnershipStakeInput = {
-    Owner: "",
-    Share: 100,
-    Locked: false
-  };
-
-  set value (val: OwnershipStakeInput)
-  {
-    // this value is updated by programmatic changes if( val !== undefined && this.val !== val){
-    if (val != null) this.OwnershipStake = val
-    this.onChange(val)
-    this.onTouch(val)
-  }
-
-  get Owner ()
-  {
-    return this.OwnershipStake.Owner;
-  }
-
-  set Owner (val: string)
-  {
-    this.OwnershipStake.Owner = val;
-    this.onChange(this.OwnershipStake)
-    this.onTouch(this.OwnershipStake)
-  }
-
-  get Share ()
-  {
-    return this.OwnershipStake.Share;
-  }
-
-  set Share (val: number)
-  {
-    this.OwnershipStake.Share = val;
-    this.onChange(this.OwnershipStake)
-    this.onTouch(this.OwnershipStake)
-  }
-
-  writeValue (value: any)
-  {
-    this.value = value
-  }
-
-  registerOnChange (fn: any)
-  {
-    this.onChange = fn
-  }
-
-  registerOnTouched (fn: any)
-  {
-    this.onTouch = fn
-  }
-
-  validate (control: AbstractControl): ValidationErrors | null
-  {
-    return null;
+    return false;
   }
 
   public Remove (): void
   {
-    this.DestroyEmit.emit();
+    (this.Form.parent as FormArray).removeAt(this.Index);
   }
 
   public Lock (): void
   {
-    this.OwnershipStake.Locked = true;
-    this.onChange(this.OwnershipStake)
-    this.onTouch(this.OwnershipStake)
+    this.Form.disable();
+    console.log(this.Form);
   }
 
   public UnLock (): void
   {
-    this.OwnershipStake.Locked = false;
+    this.Form.enable();
+    console.log(this.Form);
   }
 }
--- CRPL.Web/ClientApp/src/app/Forms/ownership-structure/ownership-structure.component.ts
+++ CRPL.Web/ClientApp/src/app/Forms/ownership-structure/ownership-structure.component.ts
@@ -1,125 +1,50 @@
 import {Component, OnInit} from '@angular/core';
-import {OwnershipStakeInput} from "../../_Models/StructuredOwnership/OwnershipStake";
 import {AuthService} from "../../_Services/auth.service";
 import {
-  AbstractControl,
-  ControlValueAccessor,
-  NG_VALIDATORS,
-  NG_VALUE_ACCESSOR,
-  ValidationErrors
+  Form,
+  FormArray, FormBuilder, FormGroup, Validators
 } from "@angular/forms";
 
 @Component({
   selector: 'ownership-structure-input',
   templateUrl: './ownership-structure.component.html',
-  styleUrls: ['./ownership-structure.component.css'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      multi: true,
-      useExisting: OwnershipStructureComponent
-    },
-    {
-      provide: NG_VALIDATORS,
-      multi: true,
-      useExisting: OwnershipStructureComponent
-    },
-  ]
+  styleUrls: ['./ownership-structure.component.css']
 })
-export class OwnershipStructureComponent implements OnInit, ControlValueAccessor
+export class OwnershipStructureComponent implements OnInit
 {
-  public OwnershipStakes: OwnershipStakeInput[] = [];
-  public TotalShares: number = 100;
+  public Form: FormGroup;
 
-  public trackByIdx (index: number, obj: any): any
+  constructor (private authService: AuthService, private fb: FormBuilder)
   {
-    return index;
+    this.Form = fb.group({
+      TotalShares: [100, [Validators.required]],
+      Stakes: this.fb.array([this.constructStakeGroup(this.authService.UserAccount.getValue().WalletPublicAddress), this.constructStakeGroup()])
+    });
   }
 
-  onChange: any = () =>
+  private constructStakeGroup(owner: string = ''): FormGroup
   {
-  }
-
-  onTouch: any = () =>
-  {
-  }
-
-  constructor (private authService: AuthService)
-  {
+    return this.fb.group({
+      Owner: [owner, [Validators.required]],
+      Share: [1, [Validators.required]]
+    })
   }
 
   public ngOnInit (): void
   {
-    this.OwnershipStakes.push({
-      Owner: this.authService.UserAccount.getValue().WalletPublicAddress,
-      Share: this.TotalShares,
-      Locked: false
-    });
+  }
+
+  get StakeArray() : FormArray {
+    return (this.Form.controls.Stakes as FormArray)
+  }
+
+  get Stakes (): FormGroup[]
+  {
+    return this.StakeArray.controls as FormGroup[];
   }
 
   public AddStake (): void
   {
-    this.OwnershipStakes.push({Owner: "", Share: 1, Locked: false});
-    this.onTouch(this.OwnershipStakes);
-    this.onChange(this.OwnershipStakes);
-  }
-
-  public DestroyStake (i: number): void
-  {
-    this.OwnershipStakes.splice(i, 1);
-    this.onTouch(this.OwnershipStakes);
-    this.onChange(this.OwnershipStakes);
-  }
-
-  public IsOwnershipValid (): boolean
-  {
-    let count = 0;
-    for (let ownershipStake of this.OwnershipStakes)
-    {
-      count += ownershipStake.Share;
-    }
-    return count == this.TotalShares;
-  }
-
-  public CalculateMaximumShares (i: number): number
-  {
-    if (this.OwnershipStakes[i].Locked) return this.OwnershipStakes[i].Share;
-
-    let areSomeLocked = this.OwnershipStakes.find(x => x.Locked);
-
-    if (areSomeLocked)
-    {
-      let lockedShares = this.OwnershipStakes.filter(x => x.Locked).map(x => x.Share);
-      let totalLockedShares = lockedShares.reduce((sum, current) => sum + current, 0);
-      return (this.TotalShares - totalLockedShares) - ((this.OwnershipStakes.length - lockedShares.length) - 1);
-    }
-
-    return this.TotalShares - (this.OwnershipStakes.length - 1);
-  }
-
-  writeValue (value: any)
-  {
-    // this.OwnershipStakes = value
-  }
-
-  registerOnChange (fn: any)
-  {
-    this.onChange = fn
-  }
-
-  registerOnTouched (fn: any)
-  {
-    this.onTouch = fn
-  }
-
-  validate (control: AbstractControl): ValidationErrors | null
-  {
-    return null;
-  }
-
-  public StakeChanged (index: number): void
-  {
-    this.onTouch(this.OwnershipStakes);
-    this.onChange(this.OwnershipStakes);
+    this.StakeArray.push(this.constructStakeGroup());
   }
 }
