8809d2fcc62d57da6ddc94417fbc0506a3b12536
Harrison Barker <harrison@thebarkers.me.uk>
2022-02-06 21:32:19 +0000

    Share structure validation
    CRPL-104 Ownership structure form

--- CRPL.Web/ClientApp/src/app/Forms/ownership-structure/ownership-structure.component.ts
+++ CRPL.Web/ClientApp/src/app/Forms/ownership-structure/ownership-structure.component.ts
@@ -1,9 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../_Services/auth.service";
 import {
+  AbstractControl,
   Form,
-  FormArray, FormBuilder, FormGroup, Validators
+  FormArray, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators
 } from "@angular/forms";
+
+function ShareStructureValidator() : ValidatorFn
+{
+  return (control: AbstractControl): ValidationErrors | null =>
+  {
+    let totalShares = control.parent?.value.TotalShares;
+    let count = 0;
+    (control as FormArray).controls.forEach(c => count += c.value.Share);
+    return count == totalShares ? null : {'InvalidShareStructure': true};
+  }
+}
 
 @Component({
   selector: 'ownership-structure-input',
@@ -18,11 +30,11 @@ export class OwnershipStructureComponent implements OnInit
   {
     this.Form = fb.group({
       TotalShares: [100, [Validators.required]],
-      Stakes: this.fb.array([this.constructStakeGroup(this.authService.UserAccount.getValue().WalletPublicAddress), this.constructStakeGroup()])
+      Stakes: this.fb.array([this.constructStakeGroup(this.authService.UserAccount.getValue().WalletPublicAddress), this.constructStakeGroup()], [ShareStructureValidator()])
     });
   }
 
-  private constructStakeGroup(owner: string = ''): FormGroup
+  private constructStakeGroup (owner: string = ''): FormGroup
   {
     return this.fb.group({
       Owner: [owner, [Validators.required]],
@@ -34,7 +46,8 @@ export class OwnershipStructureComponent implements OnInit
   {
   }
 
-  get StakeArray() : FormArray {
+  get StakeArray (): FormArray
+  {
     return (this.Form.controls.Stakes as FormArray)
   }
 
