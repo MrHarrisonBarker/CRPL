870ba95b27b2edf1828ea70893551a1e00b7f4be
Harrison Barker <harrison@thebarkers.me.uk>
2022-02-07 19:04:50 +0000

    Copyright registration event processing
    CRPL-72 Copyright application state management

--- CRPL.Web/ClientApp/proxy.conf.js
+++ CRPL.Web/ClientApp/proxy.conf.js
@@ -8,7 +8,8 @@ const PROXY_CONFIG = [
     context: [
       "/user",
       "/works",
-      "/forms"
+      "/forms",
+      "/copyright"
    ],
     target: target,
     secure: false
--- CRPL.Web/Services/Background/EventProcessors/RegisteredEventProcessor.cs
+++ CRPL.Web/Services/Background/EventProcessors/RegisteredEventProcessor.cs
@@ -1,14 +1,33 @@
 using CRPL.Contracts.Standard.ContractDefinition;
+using CRPL.Data.Account;
+using CRPL.Data.Applications;
+using CRPL.Web.Exceptions;
+using Microsoft.EntityFrameworkCore;
 using Nethereum.Contracts;
 
 namespace CRPL.Web.Services.Background.EventProcessors;
 
 public static class RegisteredEventProcessor
 {
-    public static Task ProcessEvent(this EventLog<RegisteredEventDTO> registeredEvent, IServiceProvider serviceProvider)
+    public static async Task ProcessEvent(this EventLog<RegisteredEventDTO> registeredEvent, IServiceProvider serviceProvider)
     {
+        using var scope = serviceProvider.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+
+        var registeredWork = await context.RegisteredWorks
+            .Include(x => x.AssociatedApplication)
+            .FirstOrDefaultAsync(x => x.RegisteredTransactionId == registeredEvent.Log.TransactionHash);
+        if (registeredWork == null) throw new WorkNotFoundException(registeredEvent.Log.TransactionHash);
+
+        context.Update(registeredWork);
         
-        
-        return Task.CompletedTask;
+        registeredWork.RightId = registeredEvent.Event.RightId.ToString();
+        // TODO: can I set it to the time of the transaction?
+        registeredWork.Registered = DateTime.Now;
+        registeredWork.Status = RegisteredWorkStatus.Registered;
+
+        registeredWork.AssociatedApplication.First().Status = ApplicationStatus.Complete;
+
+        await context.SaveChangesAsync();
     }
 }
