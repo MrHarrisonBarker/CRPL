855486182175687809aef03a9ad28169186f7524
Harrison Barker <harrison@thebarkers.me.uk>
2022-02-07 02:43:14 +0000

    setup Angular forms service testbed

--- CRPL.Web/ClientApp/src/app/_Services/forms.service.spec.ts
+++ CRPL.Web/ClientApp/src/app/_Services/forms.service.spec.ts
@@ -1,16 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { FormsService } from './forms.service';
+import {FormsService} from './forms.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {UserService} from "./user.service";
+import {AuthService} from "./auth.service";
+import {AlertService} from "./alert.service";
+import {Router} from "@angular/router";
 
-describe('FormsService', () => {
+describe('FormsService', () =>
+{
+
   let service: FormsService;
+  let alertMock = jasmine.createSpyObj('alertMock', ['Alert']);
+  let authMock = jasmine.createSpyObj('AuthService', [''], ['IsAuthenticated', 'UserAccount'])
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(() =>
+  {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        FormsService,
+        {provide: AuthService, useValue: authMock},
+        {provide: AlertService, useValue: alertMock},
+        {provide: 'BASE_URL', useValue: ''}
+      ]
+    });
     service = TestBed.inject(FormsService);
   });
 
-  it('should be created', () => {
+  it('should be created', () =>
+  {
     expect(service).toBeTruthy();
   });
 });
