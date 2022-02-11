4130f65569ec9a2dcbc1ff205221736efd9aa8a2
Harrison Barker <harrison@thebarkers.me.uk>
2022-02-11 00:04:11 +0000

    angular testbed providers

--- CRPL.Web/ClientApp/src/app/_Services/copyright.service.spec.ts
+++ CRPL.Web/ClientApp/src/app/_Services/copyright.service.spec.ts
@@ -1,16 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { CopyrightService } from './copyright.service';
+import {CopyrightService} from './copyright.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {AuthService} from "./auth.service";
+import {AlertService} from "./alert.service";
+import {WarehouseService} from "./warehouse.service";
 
-describe('CopyrightService', () => {
+describe('CopyrightService', () =>
+{
   let service: CopyrightService;
+  let alertMock = jasmine.createSpyObj('alertMock', ['Alert']);
+  let authMock = jasmine.createSpyObj('AuthService', [''], ['IsAuthenticated', 'UserAccount']);
+  let warehouseMock = jasmine.createSpyObj('WarehouseService', [''])
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(() =>
+  {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        CopyrightService,
+        {provide: AuthService, useValue: authMock},
+        {provide: AlertService, useValue: alertMock},
+        {provide: WarehouseService, useValue: warehouseMock},
+        {provide: 'BASE_URL', useValue: ''}]
+    });
     service = TestBed.inject(CopyrightService);
   });
 
-  it('should be created', () => {
+  it('should be created', () =>
+  {
     expect(service).toBeTruthy();
   });
 });
