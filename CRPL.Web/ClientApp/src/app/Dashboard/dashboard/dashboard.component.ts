1173f5624ad945b062eee44d8c6c4bfa3ba263d7
Harrison Barker <harrison@thebarkers.me.uk>
2022-02-13 16:29:14 +0000

    navigation to dashboard from work if current user is apart of the ownership structure. Dashboard can now handle work or application id route parameter to open selected.
    CRPL-106 Management link if own

--- CRPL.Web/ClientApp/src/app/Dashboard/dashboard/dashboard.component.ts
+++ CRPL.Web/ClientApp/src/app/Dashboard/dashboard/dashboard.component.ts
@@ -7,6 +7,7 @@ import {ApplicationStatus} from "../../_Models/Applications/ApplicationStatus";
 import {forkJoin} from "rxjs";
 import {AlertService} from "../../_Services/alert.service";
 import {WarehouseService} from "../../_Services/warehouse.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
@@ -23,19 +24,31 @@ export class DashboardComponent implements OnInit
     private formsService: FormsService,
     private copyrightService: CopyrightService,
     private alertService: AlertService,
-    private warehouse: WarehouseService)
+    private warehouse: WarehouseService,
+    private route: ActivatedRoute)
   {
   }
 
-  ngOnInit (): void
+  async ngOnInit (): Promise<any>
   {
     this.alertService.StartLoading();
-    forkJoin([this.formsService.GetMyApplications(), this.copyrightService.GetMyCopyrights()]).subscribe(x =>
+    // GET EVERYTHING
+    await forkJoin([this.formsService.GetMyApplications(), this.copyrightService.GetMyCopyrights()]).subscribe(x =>
     {
       console.log(this.warehouse.MyApplications, this.warehouse.MyWorks);
+
+      // ROUTE WORK
+      if (this.route.snapshot.paramMap.has("workId")) this.Selected = this.warehouse.MyWorks.find(x => x.Id == this.route.snapshot.paramMap.get("workId")) as RegisteredWorkViewModel
+
+      // ROUTE APPLICATION
+      if (this.route.snapshot.paramMap.has("applicationId")) this.Selected = this.warehouse.MyApplications.find(x => x.Id == this.route.snapshot.paramMap.get("applicationId")) as ApplicationViewModel
+
       this.Loaded = true;
       this.alertService.StopLoading();
     });
+
+    console.log(this.route.snapshot.paramMap.keys);
+
   }
 
   get RegisteredCopyrights (): RegisteredWorkViewModel[]
--- CRPL.Web/ClientApp/src/app/copyright/copyright.component.ts
+++ CRPL.Web/ClientApp/src/app/copyright/copyright.component.ts
@@ -1,9 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {CopyrightService} from "../_Services/copyright.service";
 import {RegisteredWorkViewModel} from "../_Models/Works/RegisteredWork";
 import {ClarityIcons, undoIcon} from "@cds/core/icon";
 import {Location} from "@angular/common";
+import {AuthService} from "../_Services/auth.service";
+import {AlertService} from "../_Services/alert.service";
 
 @Component({
   selector: 'app-copyright',
@@ -14,19 +16,45 @@ export class CopyrightComponent implements OnInit
 {
   public Copyright!: RegisteredWorkViewModel;
 
-  constructor (private route: ActivatedRoute, private copyrightService: CopyrightService, private location: Location)
+  constructor (
+    private route: ActivatedRoute,
+    private copyrightService: CopyrightService,
+    private location: Location,
+    private authService: AuthService,
+    private router: Router,
+    private alertService: AlertService)
   {
     ClarityIcons.addIcons(undoIcon);
   }
 
   async ngOnInit (): Promise<any>
   {
+    this.alertService.StartLoading();
     let workId = this.route.snapshot.paramMap.get('id');
-    if (workId) await this.copyrightService.Get(workId).subscribe(x => this.Copyright = x);
+    if (workId) await this.copyrightService.Get(workId).subscribe(x =>
+    {
+      this.Copyright = x;
+      this.alertService.StopLoading();
+    });
   }
 
-  Back ()
+  public IsOwner (): boolean
+  {
+    if (!this.Copyright?.OwnershipStructure) return false;
+    if (this.authService.UserAccount.getValue())
+    {
+      return this.Copyright.OwnershipStructure?.findIndex(x => x.Owner.toLowerCase() == this.authService.UserAccount.getValue().WalletPublicAddress.toLowerCase()) > -1;
+    }
+    return false;
+  }
+
+  public Back (): void
   {
     this.location.back();
+  }
+
+  public NavigateToDashboard (): void
+  {
+    this.router.navigate(['/dashboard', {workId: this.Copyright.Id}]);
   }
 }
