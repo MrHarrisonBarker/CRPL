9fe62164660cd98b7b498109b8d70808fd5d873e
Harrison Barker <harrison@thebarkers.me.uk>
2022-02-14 23:13:41 +0000

    Factory for creating a service provider with injected Application context

---
+++ CRPL.Tests/Factories/ServiceProviderFactory.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using CRPL.Data.Account;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+
+namespace CRPL.Tests.Factories;
+
+[TestFixture]
+public class ServiceProviderWithContextFactory
+{
+    public async Task<(ApplicationContext, IServiceProvider)> Create()
+    {
+        var context = new TestDbApplicationContextFactory().CreateContext();
+        
+        var serviceProvider = new Mock<IServiceProvider>();
+        serviceProvider.Setup(x => x.GetService(typeof(ApplicationContext))).Returns(context);
+
+        var serviceScope = new Mock<IServiceScope>();
+        serviceScope.Setup(x => x.ServiceProvider).Returns(serviceProvider.Object);
+
+        var serviceScopeFactory = new Mock<IServiceScopeFactory>();
+        serviceScopeFactory.Setup(x => x.CreateScope()).Returns(serviceScope.Object);
+
+        serviceProvider.Setup(x => x.GetService(typeof(IServiceScopeFactory))).Returns(serviceScopeFactory.Object);
+        
+        return (context, serviceProvider.Object);
+    }
+}
