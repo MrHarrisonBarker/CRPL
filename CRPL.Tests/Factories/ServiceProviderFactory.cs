7fea625090afcaca4dbff3d7cab53154772c2493
Harrison Barker <harrison@thebarkers.me.uk>
2022-02-18 00:14:11 +0000

    fixed test service provider

--- CRPL.Tests/Factories/ServiceProviderFactory.cs
+++ CRPL.Tests/Factories/ServiceProviderFactory.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using CRPL.Data.Account;
+using CRPL.Web.Services.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 
@@ -8,28 +9,18 @@ namespace CRPL.Tests.Factories;
 
 public class ServiceProviderWithContextFactory
 {
-    public async Task<(ApplicationContext, IServiceProvider)> Create()
+    public async Task<(ApplicationContext, IServiceProvider)> Create(ApplicationContext context = null)
     {
-        var context = new TestDbApplicationContextFactory().CreateContext();
-
+        if (context == null)
+        {
+            context = new TestDbApplicationContextFactory().CreateContext();
+        }
+        
+        var worksVerificationServiceMock = new Mock<IWorksVerificationService>();
+        
         var serviceProvider = new Mock<IServiceProvider>();
         serviceProvider.Setup(x => x.GetService(typeof(ApplicationContext))).Returns(context);
-
-        var serviceScope = new Mock<IServiceScope>();
-        serviceScope.Setup(x => x.ServiceProvider).Returns(serviceProvider.Object);
-
-        var serviceScopeFactory = new Mock<IServiceScopeFactory>();
-        serviceScopeFactory.Setup(x => x.CreateScope()).Returns(serviceScope.Object);
-
-        serviceProvider.Setup(x => x.GetService(typeof(IServiceScopeFactory))).Returns(serviceScopeFactory.Object);
-
-        return (context, serviceProvider.Object);
-    }
-
-    public async Task<(ApplicationContext, IServiceProvider)> Create(ApplicationContext context)
-    {
-        var serviceProvider = new Mock<IServiceProvider>();
-        serviceProvider.Setup(x => x.GetService(typeof(ApplicationContext))).Returns(context);
+        serviceProvider.Setup(x => x.GetService(typeof(IWorksVerificationService))).Returns(worksVerificationServiceMock.Object);
 
         var serviceScope = new Mock<IServiceScope>();
         serviceScope.Setup(x => x.ServiceProvider).Returns(serviceProvider.Object);
