583ba7177d3d133fc6a38cd8d11fda0fb9ee25f2
Harrison Barker <harrison@thebarkers.me.uk>
2022-02-25 01:55:09 +0000

    Added state to the resolve result
    CRPL-147 Resolve transaction

--- CRPL.Data/Applications/Core/ResolveResult.cs
+++ CRPL.Data/Applications/Core/ResolveResult.cs
@@ -6,13 +6,24 @@ namespace CRPL.Data.Applications.Core;
 public class ResolveResult
 {
     public bool Rejected { get; set; }
+    public ResolveStatus ResolvedStatus { get; set; }
     public string? Transaction { get; set; }
     public string? Message { get; set; }
+}
+
+public enum ResolveStatus
+{
+    Created,
+    NeedsOnChainAction,
+    Processing,
+    Resolved,
+    Failed
 }
 
 public class ResolveResultWithUri
 {
     public bool Rejected { get; set; }
+    public ResolveStatus ResolvedStatus { get; set; }
     public string? Transaction { get; set; }
     public string? TransactionUri { get; set; }
     public string? Message { get; set; }
---
+++ CRPL.Web/Migrations/Application/20220225015132_resolveStatus.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRPL.Web.Migrations.Application
+{
+    public partial class resolveStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ResolveResult_ResolvedStatus",
+                table: "Applications",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResolveResult_ResolvedStatus",
+                table: "Applications");
+        }
+    }
+}
