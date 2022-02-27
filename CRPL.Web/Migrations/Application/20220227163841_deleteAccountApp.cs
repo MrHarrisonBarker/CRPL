7c80559de2a9710187786379bce9e847f6261482
Harrison Barker <harrison@thebarkers.me.uk>
2022-02-27 16:39:27 +0000

    Delete account application migration

---
+++ CRPL.Web/Migrations/Application/20220227163841_deleteAccountApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRPL.Web.Migrations.Application
+{
+    public partial class deleteAccountApp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AccountId",
+                table: "Applications",
+                type: "char(36)",
+                nullable: true,
+                collation: "ascii_general_ci");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "Applications");
+        }
+    }
+}
