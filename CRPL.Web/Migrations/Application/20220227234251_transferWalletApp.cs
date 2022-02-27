fd7586b91dd4ea28650da12eaff28e065b033faa
Harrison Barker <harrison@thebarkers.me.uk>
2022-02-27 23:44:55 +0000

    Wallet transfer application db migration

---
+++ CRPL.Web/Migrations/Application/20220227234251_transferWalletApp.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRPL.Web.Migrations.Application
+{
+    public partial class transferWalletApp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "WalletAddress",
+                table: "Applications",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WalletAddress",
+                table: "Applications");
+        }
+    }
+}
