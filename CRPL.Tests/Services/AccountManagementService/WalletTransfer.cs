bc896b8552c31983618ae87ca8ffb23d508f5c5e
Harrison Barker <harrison@thebarkers.me.uk>
2022-02-28 21:33:20 +0000

    account management service test fix

--- CRPL.Tests/Services/AccountManagementService/WalletTransfer.cs
+++ CRPL.Tests/Services/AccountManagementService/WalletTransfer.cs
@@ -5,9 +5,12 @@ using System.Threading.Tasks;
 using CRPL.Data.Account;
 using CRPL.Data.Applications;
 using CRPL.Data.Applications.DataModels;
+using CRPL.Data.Applications.InputModels;
+using CRPL.Data.Applications.ViewModels;
 using CRPL.Tests.Factories;
 using CRPL.Tests.Mocks;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using NUnit.Framework;
 
 namespace CRPL.Tests.Services.AccountManagementService;
@@ -61,7 +64,11 @@ public class WalletTransfer
                     {
                         RegisteredWork = Works.First()
                     }
-                }
+                },
+                Applications = Applications.Select(x => new UserApplication()
+                {
+                    Application = x
+                }).ToList()
             }
         };
     }
@@ -69,17 +76,25 @@ public class WalletTransfer
     [Test]
     public async Task Should_Propose()
     {
-        await using (var context = new TestDbApplicationContextFactory().CreateContext(Works, userAccounts: UserAccounts))
+        await using (var context = new TestDbApplicationContextFactory().CreateContext(Works, Applications, UserAccounts))
         {
             var mappings = MockWebUtils.DefaultMappings;
             mappings["eth_call"] =
                 "0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000200000000000000000000000012890d2cce102216644c59dae5baed380d84830c0000000000000000000000000000000000000000000000000000000000000037000000000000000000000000aea270413700371a8a28ab8b5ece05201bdf49de000000000000000000000000000000000000000000000000000000000000002d";
             
             var (accountManagementService, formsServiceMock) = new AccountManagementServiceFactory().Create(context, mappings);
+
+            formsServiceMock.Setup(x => x.Update<OwnershipRestructureViewModel>(It.IsAny<ApplicationInputModel>())).Returns<ApplicationInputModel>((inputModel) =>
+                Task.FromResult(new OwnershipRestructureViewModel()
+                {
+                    Id = new Guid("186A2071-525D-4347-AF09-129745623C15")
+                }));
             
-            var application = await context.WalletTransferApplications.FirstOrDefaultAsync(x => x.Id == new Guid("DB27D402-B34E-42AE-AC6E-054AF46EB04A"));
+            var application = await context.WalletTransferApplications.Include(x => x.AssociatedUsers).FirstOrDefaultAsync(x => x.Id == new Guid("DB27D402-B34E-42AE-AC6E-054AF46EB04A"));
 
             await accountManagementService.WalletTransfer(application);
+            formsServiceMock.Verify(x => x.Update<OwnershipRestructureViewModel>(It.IsAny<ApplicationInputModel>()), Times.Once);
+            formsServiceMock.Verify(x => x.Submit<OwnershipRestructureApplication, OwnershipRestructureViewModel>(new Guid("186A2071-525D-4347-AF09-129745623C15")), Times.Once);
         }
     }
 }
