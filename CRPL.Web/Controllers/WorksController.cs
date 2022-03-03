749e53a906d52f6ddfeadef454c554f9380d535d
Harrison Barker <harrison@thebarkers.me.uk>
2022-03-03 03:46:09 +0000

    Ping endpoint records usage.
    CRPL-129 Ping webhook/endpoint

--- CRPL.Web/Controllers/WorksController.cs
+++ CRPL.Web/Controllers/WorksController.cs
@@ -1,3 +1,5 @@
+using CRPL.Data.Account.Works;
+using CRPL.Web.Services.Background.Usage;
 using CRPL.Web.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,11 +11,13 @@ public class WorksController : ControllerBase
 {
     private readonly ILogger<UserController> Logger;
     private readonly IWorksVerificationService WorksVerificationService;
+    private readonly IUsageQueue UsageQueue;
 
-    public WorksController(ILogger<UserController> logger, IWorksVerificationService worksVerificationService)
+    public WorksController(ILogger<UserController> logger, IWorksVerificationService worksVerificationService, IUsageQueue usageQueue)
     {
         Logger = logger;
         WorksVerificationService = worksVerificationService;
+        UsageQueue = usageQueue;
     }
 
     [HttpPost, DisableRequestSizeLimit]
@@ -33,19 +37,23 @@ public class WorksController : ControllerBase
         }
     }
 
-    // [HttpGet("{id}")]
-    // public async Task<IActionResult> GetSignedWork(Guid id)
-    // {
-    //     try
-    //     {
-    //         var work = await WorksVerificationService.GetSigned(id);
-    //
-    //         return File(work.Work, work.ContentType, work.FileName);
-    //     }
-    //     catch (Exception e)
-    //     {
-    //         Logger.LogError(e, "Exception thrown when getting signed work");
-    //         throw;
-    //     }
-    // }
+    [HttpPatch("ping/{id}")]
+    public async Task<IActionResult> Ping(Guid id)
+    {
+        try
+        {
+            UsageQueue.QueueUsage(new WorkUsage
+            {
+                WorkId = id,
+                TimeStamp = DateTime.Now,
+                UsageType = UsageType.Ping
+            });
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            Logger.LogError(e, "Exception thrown when pinging");
+            throw;
+        }
+    }
 }
