82592aff6ba0154835621d37b80d46bef77d60ef
Harrison Barker <harrison@thebarkers.me.uk>
2022-03-04 05:13:56 +0000

    Application view cancel button locking.
    CRPL-158 Disable inputs and buttons

--- CRPL.Web/ClientApp/src/app/Dashboard/applications-view/applications-view.component.ts
+++ CRPL.Web/ClientApp/src/app/Dashboard/applications-view/applications-view.component.ts
@@ -8,6 +8,7 @@ import {AlertService} from "../../_Services/alert.service";
 import {FormsService} from "../../_Services/forms.service";
 import {ClarityIcons, trashIcon} from "@cds/core/icon";
 import {DisputeViewModel} from "../../_Models/Applications/DisputeViewModel";
+import {finalize} from "rxjs/operators";
 
 @Component({
   selector: 'applications-view [Application] [ShowForms]',
@@ -19,6 +20,7 @@ export class ApplicationsViewComponent implements OnInit
   @Input() Application!: ApplicationViewModel;
   @Input() ShowForms: boolean = false;
   @Input() Cancelable: boolean = true;
+  public Locked: boolean = false;
 
   constructor (private warehouse: WarehouseService, private alertService: AlertService, private formsService: FormsService)
   {
@@ -36,35 +38,40 @@ export class ApplicationsViewComponent implements OnInit
     return false;
   }
 
-  get ApplicationAsCopyrightRegistration () : CopyrightRegistrationViewModel
+  get ApplicationAsCopyrightRegistration (): CopyrightRegistrationViewModel
   {
     return (this.Application as CopyrightRegistrationViewModel);
   }
 
-  get ApplicationAsOwnershipRestructure() : OwnershipRestructureViewModel
+  get ApplicationAsOwnershipRestructure (): OwnershipRestructureViewModel
   {
     return (this.Application as OwnershipRestructureViewModel);
   }
 
-  get ApplicationAsDispute() : DisputeViewModel
+  get ApplicationAsDispute (): DisputeViewModel
   {
     return (this.Application as DisputeViewModel);
   }
 
-  get ExistingWork(): RegisteredWorkViewModel
+  get ExistingWork (): RegisteredWorkViewModel
   {
     return <RegisteredWorkViewModel>this.warehouse.MyWorks.find(x => x.Id == this.Application.AssociatedWork?.Id);
   }
 
   public Cancel (): void
   {
-
-      this.formsService.Cancel(this.Application.Id).subscribe(x =>
-      {
-        this.alertService.Alert({Type: 'success', Message: 'Canceled application'});
-        this.Application = null as any;
-        this.alertService.TriggerChange.emit();
-      });
+    this.Locked = true;
+    this.formsService.Cancel(this.Application.Id)
+        .pipe(finalize(() => this.Locked = false))
+        .subscribe(x =>
+        {
+          this.alertService.Alert({Type: 'success', Message: 'Canceled application'});
+          this.Application = null as any;
+          this.alertService.TriggerChange.emit();
+        }, error => this.alertService.Alert({
+          Type: 'danger',
+          Message: 'There was a problem when canceling this application'
+        }));
 
   }
 }
