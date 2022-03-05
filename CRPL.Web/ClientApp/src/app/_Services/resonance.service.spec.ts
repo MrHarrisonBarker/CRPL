a057822f0319bd7a9a30bc3b3d7f7795962062aa
Harrison Barker <harrison@thebarkers.me.uk>
2022-03-05 10:35:24 +0000

    Angular resonance service test bed config

--- CRPL.Web/ClientApp/src/app/_Services/resonance.service.spec.ts
+++ CRPL.Web/ClientApp/src/app/_Services/resonance.service.spec.ts
@@ -1,12 +1,21 @@
 import {TestBed} from '@angular/core/testing';
 
 import {ResonanceService} from './resonance.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {WarehouseService} from "./warehouse.service";
 
 describe('ResonanceService', () => {
   let service: ResonanceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        ResonanceService,
+        WarehouseService,
+        {provide: 'BASE_URL', useValue: ''}
+      ]
+    });
     service = TestBed.inject(ResonanceService);
   });
 
